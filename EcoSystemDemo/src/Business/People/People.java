99bef8cbb2b92b963d47531d9c00d12c968fc9eb
TejalBhalerao <bhalerao.t@northeastern.edu>
2021-04-22 16:43:49 -0400

    Tejal commits

--- EcoSystemDemo/src/Business/People/People.java
+++ EcoSystemDemo/src/Business/People/People.java
@@ -3,12 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Business.Role;
+package Business.People;
+
+import java.util.Date;
 
 /**
  *
  * @author TEJAL
  */
-public class SystemAdminRole {
+public class People {
     
-}
+   
+  
+ }
+
